615e403252b88dff9b8330723dcf8e6c3dc24fc4
LEE-SungBin <rqtoe@snu.ac.kr>
2023-07-06 20:51:11 +0900

    data added

--- HOTRG/main.py
+++ HOTRG/main.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 if __name__ == "__main__":
     sys.path.append(str(Path(__file__).parents[1]))
-    print(f"{sys.path}")
+    # print(f"{sys.path}")
 
     from HOTRG.src.multiprocessing import multiprocessing
 
